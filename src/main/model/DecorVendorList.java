65ff18130d266cc7ff250245b39c787114e10492
avaniak <avani.karadkhedkar@gmail.com>
2021-02-13 23:07:03 -0800

    Version 2

--- src/main/model/DecorVendorList.java
+++ src/main/model/DecorVendorList.java
@@ -1,17 +1,22 @@
 package model;
 
-import com.sun.org.apache.xpath.internal.objects.XString;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class DecorVendorList {
     ArrayList<String> decorList = new ArrayList<>();
 
+    // MODIFIES: this
+    // EFFECTS: helped function for adding decor vendor name to list
     public ArrayList<String> addVendors(String vendorName) {
         decorList.add(vendorName);
         return decorList;
+    }
 
+    // MODIFIES: this
+    // EFFECTS: helper function for deleting decor vendor name from list
+    public ArrayList<String> removeVendors(String vendorName) {
+        decorList.remove(vendorName);
+        return decorList;
     }
 
 }
